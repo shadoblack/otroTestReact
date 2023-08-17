f085eac8e6ac4f88c421aa1619871caf84b60f08
unknown <edgarerb@hotmail.es>
2023-08-17 14:56:06 +0200

    test de useState

--- projects/test01/src/App.jsx
+++ projects/test01/src/App.jsx
@@ -1,15 +1,19 @@
 import "./App.css";
 import { FollowCard } from "./FollowCard";
+import { useState } from "react";
 
 export function App() {
+   const [name,setName] = useState('Karen')
   return (
     <section className="App">
-      <FollowCard isFollowing={true} userName="omidnikrah">
+      <FollowCard isFollowing={true} userName={name}>
         Gran
       </FollowCard>
       <FollowCard isFollowing={false} userName="omidnikrah">
         Torino
       </FollowCard>
+
+      <button onClick={() => setName('Wilfreed')}>Cambio de nombre</button>
    
     </section>
   );
--- projects/test01/src/FollowCard.jsx
+++ projects/test01/src/FollowCard.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import "./App.css";
 
-export function FollowCard({ children, userName, isFollowing }) {
+export function FollowCard({ children, userName, initialIsFollowing }) {
+  /* estados del useState y lo que devuelve,manera larga y no buena practica, la buena es la de abajo.
+const state = useState(false)
+const isFollowing = state[0]
+const setIsFollowing = state[1]*/
+
+  const [isFollowing, setIsFollowing] = useState(initialIsFollowing);
+
   const text = isFollowing ? "siguiendo" : "seguir";
   const buttonClassName = isFollowing
     ? "test-followCard-button is-following"
     : "test-followCard-button";
+
+  const handleClick = () => {
+    setIsFollowing(!isFollowing);
+  };
   return (
     <article className="test-followCard">
       <header className="test-followCard-header">
@@ -19,7 +31,9 @@ export function FollowCard({ children, userName, isFollowing }) {
         </div>
       </header>
       <aside>
-        <button className={buttonClassName}>{text}</button>
+        <button className={buttonClassName} onClick={handleClick}>
+          {text}
+        </button>
       </aside>
     </article>
   );
